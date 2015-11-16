748b2d580e6edc725564144c3154426702219a19
Takashi Nishimura <takashi@user.email>
2015-11-16 04:59:43 +0000

    quantity to line_items

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,20 @@
 class StoreController < ApplicationController
   def index
     @products = Product.all.order(:title)
-  end
+
+    if session[:counter].nil?
+      session[:counter] = 1
+    else
+      session[:counter] += 1
+    end
+
+    @counter = session[:counter]
+
+  #   # puts "-------------"
+  #   # puts session[:counter]
+  #   # puts "-------------"
+
+  #   # session[:counter] = 1
+end
 
 end
